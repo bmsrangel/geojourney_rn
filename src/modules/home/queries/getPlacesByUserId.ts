12b365406fb28fbc8a521b3b6a5a9c4921aa00f6
bmsrangel <bmsrangel@hotmail.com>
2023-01-18 11:34:55 -0300

    Updated getPlacesByUserId query to retrieve data ordered by date descendant

--- src/modules/home/queries/getPlacesByUserId.ts
+++ src/modules/home/queries/getPlacesByUserId.ts
@@ -2,7 +2,7 @@ import {gql} from '../../../shared/utils/apolloClient';
 
 export const getPlacesByUserId = gql`
   query GetAllPlaces($userId: uuid) {
-    favorite_places(where: {user_id: {_eq: $userId}}) {
+    favorite_places(where: {user_id: {_eq: $userId}}, order_by: {date: desc}) {
       id
       date
       name
