030304c3a51364fe9bda3443d83f70cf53647e40
bmsrangel <bmsrangel@hotmail.com>
2023-01-18 11:32:27 -0300

    Updated CustomMarkerComponent to be Dark Mode sensible

--- src/modules/home/components/CustomMarkerComponent.tsx
+++ src/modules/home/components/CustomMarkerComponent.tsx
@@ -1,12 +1,17 @@
 import {Box, Text} from 'native-base';
 import React from 'react';
-import {primaryColor} from '../../../shared/constants/colors';
+import {useAppSelector} from '../../../appStore';
+import {primaryColor, primaryColorDark} from '../../../shared/constants/colors';
 
 type CustomMarkerComponentProps = {
   name: string;
 };
 
 export const CustomMarkerComponent = ({name}: CustomMarkerComponentProps) => {
+  const isDarkThemeSelected = useAppSelector(
+    state => state.app.isDarkThemeSelected,
+  );
+
   return (
     <Box
       width="30px"
@@ -14,7 +19,7 @@ export const CustomMarkerComponent = ({name}: CustomMarkerComponentProps) => {
       justifyContent="center"
       alignItems="center"
       borderRadius="20px"
-      backgroundColor={primaryColor}>
+      backgroundColor={isDarkThemeSelected ? primaryColorDark : primaryColor}>
       <Text fontSize="8px" fontWeight="500" textAlign="center" color="#fff">
         {name}
       </Text>
