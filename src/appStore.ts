b90448e7623d146b5ebf53a8c77619ba33aa33a1
bmsrangel <bmsrangel@hotmail.com>
2023-01-18 12:23:45 -0300

    Implemented redux-persist

--- src/appStore.ts
+++ src/appStore.ts
@@ -3,16 +3,53 @@ import {appReducer} from './appSlice';
 import {useDispatch, useSelector, TypedUseSelectorHook} from 'react-redux';
 import {placesListReducer} from './modules/home/slices/placesListSlice';
 import {userReducer} from './modules/home/slices/userSlice';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from 'redux-persist';
 
 export {Provider as AppStoreProvider} from 'react-redux';
 export {appActions} from './appSlice';
 
+const persistConfig = {
+  storage: AsyncStorage,
+};
+
+const persistedAppReducer = persistReducer(
+  {
+    ...persistConfig,
+    key: 'app',
+    blacklist: ['isLoading', 'coordinate'],
+  },
+  appReducer,
+);
+
+const persistedUserReducer = persistReducer(
+  {
+    ...persistConfig,
+    key: 'user',
+  },
+  userReducer,
+);
+
 export const appStore = configureStore({
   reducer: {
-    app: appReducer,
+    app: persistedAppReducer,
     placesList: placesListReducer,
-    user: userReducer,
+    user: persistedUserReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export type AppStore = ReturnType<typeof appStore.getState>;
