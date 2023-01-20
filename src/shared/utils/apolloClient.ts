75902fbd5cdea8226790aa2371f25999f5d1e02e
bmsrangel <bmsrangel@hotmail.com>
2023-01-20 18:08:45 -0300

    Refactored apolloClient to remove development code

--- src/shared/utils/apolloClient.ts
+++ src/shared/utils/apolloClient.ts
@@ -7,24 +7,13 @@ export {
   useMutation,
 } from '@apollo/client';
 
-const isDev = process.env.NODE_ENV;
-const developmentUrl = 'http://192.168.175.56:8080/v1/graphql';
-// const developmentUrl = 'http://10.0.2.2:8080/v1/graphql';
 const productionUrl = 'https://right-mustang-74.hasura.app/v1/graphql';
 
 export const apolloClient = new ApolloClient({
-  uri: isDev ? developmentUrl : productionUrl,
+  uri: productionUrl,
   headers: {
     'x-hasura-admin-secret':
       'dP7Tl6eDrderB7zW3ciu5OSRhPK4VYeIRJcet3n91qJI9Z3EkVCnBVIcdPrcWN5A',
   },
   cache: new InMemoryCache(),
 });
-// export const apolloClient = new ApolloClient({
-//   uri: 'https://right-mustang-74.hasura.app/v1/graphql',
-//   headers: {
-//     'x-hasura-admin-secret':
-//       'dP7Tl6eDrderB7zW3ciu5OSRhPK4VYeIRJcet3n91qJI9Z3EkVCnBVIcdPrcWN5A',
-//   },
-//   cache: new InMemoryCache(),
-// });
